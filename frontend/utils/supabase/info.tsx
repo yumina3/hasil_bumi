e72748d883275cb6545be172e1c4d393c369c593
yumina3 <yunamikanda06@gmail.com>
2026-08-03 21:35:00 +0700

    refactor: revert monorepo to standard flat structure for easier deployment

--- frontend/utils/supabase/info.tsx
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const projectId = "ppxtvcmbebzcsjaesyqe"
-export const publicAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBweHR2Y21iZWJ6Y3NqYWVzeXFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNjE1MTcsImV4cCI6MjA5MTgzNzUxN30.SR3dXAAxg5jfSROL6oA7njFhsLW9zxvVjUQo60TwGZA"
-
-export const supabase = createClient(`https://${projectId}.supabase.co`, publicAnonKey)
